9683597280b46e37d8ae001696d43c10dd304334
Han Jiang <hjiang4@apple.com>
2020-09-30 15:44:06 -0700

    create Router.tsx file

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -6,39 +6,11 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import debug from 'debug';
-import {
-    BrowserRouter as Router,
-    Redirect, Route, Switch,
-} from 'react-router-dom';
 
-import { withRouter } from 'react-router';
-
-import ItemList from './ItemList';
-
-import IssueEdit from './IssueEdit';
-import Navbar from './Navbar';
+import RoutedApp from './Router';
 import '../scss/App.scss';
 
 const contentNode = document.getElementById('contents');
-const NoMatch = () => <p>Page Not Found</p>;
-
-const RoutedApp = () => (
-    <div>
-        <Navbar />
-        <Router>
-            <Switch>
-                <Route
-                    exact
-                    path="/"
-                    render={() => (<Redirect to={{ pathname: '/issues' }} />)}
-                />
-                <Route exact path="/issues" component={withRouter(ItemList)} />
-                <Route exact path="/issues/:id" component={IssueEdit} />
-                <Route path="*" component={NoMatch} />
-            </Switch>
-        </Router>
-    </div>
-);
 
 debug.enable('app:*');
 
---
+++ src/tsx/Router.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import {
+    BrowserRouter as Router,
+    Redirect, Route, Switch,
+} from 'react-router-dom';
+
+import { withRouter } from 'react-router';
+
+import Navbar from './Navbar';
+
+import ItemList from './ItemList';
+
+import IssueEdit from './IssueEdit';
+
+const NoMatch = () => <p>Page Not Found</p>;
+
+const RoutedApp = () => (
+    <div>
+        <Navbar />
+        <Router>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    render={() => (<Redirect to={{ pathname: '/issues' }} />)}
+                />
+                <Route exact path="/issues" component={withRouter(ItemList)} />
+                <Route exact path="/issues/:id" component={IssueEdit} />
+                <Route path="*" component={NoMatch} />
+            </Switch>
+        </Router>
+    </div>
+);
+
+export default RoutedApp;
