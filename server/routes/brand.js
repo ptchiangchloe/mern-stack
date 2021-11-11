25a9cdce3f90321805cc72e1c321bfe3d2cbd430
Hanyu Jiang <hanyujiang5@gmail.com>
2021-11-10 17:02:41 -0800

    integrate getOrSetCache function into /api/photos/:id api call with redis cache

--- server/routes/brand.js
+++ server/routes/brand.js
@@ -15,7 +15,7 @@ router.get('/api/brands', brand_controller.brand_list);
 router.get('/api/photos', async (req, res) => {
     const albumId = req.query.albumId
     const redisKey = `photos?albumId=${albumId}`
-    const await getOrSetCache(redisKey, () => {
+    const photos = await getOrSetCache(redisKey, async () => {
         // calling out to an api which is always gonna be slow 
         const { data } = await axios.get(
             "https://jsonplaceholder.typicode.com/photos",
@@ -23,23 +23,7 @@ router.get('/api/photos', async (req, res) => {
         )
         return data
     })
-    // wrap everything in the redisClient get 
-    redisClient.get(`photos?albumId=${albumId}`, async (error, photos) => {
-        if(error) console.log(error)
-        if(photos != null) {
-            console.log(`Cache Hit`)
-            return res.json(JSON.parse(photos))
-        } else {
-            console.log(`Cache Miss`)
-            // calling out to an api which is always gonna be slow 
-            const { data } = await axios.get(
-                "https://jsonplaceholder.typicode.com/photos",
-                { params: { albumId } }
-            )
-            redisClient.setex(`photos?albumId=${albumId}`, DEFAULT_EXPIRATION, JSON.stringify(data))
-            res.json(data)
-        }
-    })
+    res.json(photos)
 })
  
 function getOrSetCache(key, cb) {
@@ -48,19 +32,22 @@ function getOrSetCache(key, cb) {
             if(error) return reject(error)
             if(data != null) return resolve(JSON.parse(data))
             const freshData = await cb()
-            redisClient.setex(`photos?albumId=${albumId}`, DEFAULT_EXPIRATION, JSON.stringify(data))
+            redisClient.setex(key, DEFAULT_EXPIRATION, JSON.stringify(data))
             resolve(freshData)
         })
     })
 }
 
-router.get("/photos/:id", async (req, res) => {
-    const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/photos/${req.params.id}`,
-    )
+router.get("/api/photos/:id", async (req, res) => {
+    const redisKey = `photos:${req.params.id}`
+    const photo = await getOrSetCache(redisKey, async () => {
+        const { data } = await axios.get(
+            `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+        )
+        return data
+    })
 
-    res.json(data)
+    res.json(photo)
 })
-
 
 module.exports = router;
