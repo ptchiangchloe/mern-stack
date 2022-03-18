d4ad64ed52f83ce828914fced94ae01d0cc3e7b2
Hanyu Jiang <hanyujiang5@gmail.com>
2022-03-18 09:56:20 -0700

    refactor ItemSizeForm file

--- src/jsx/shared/ItemSizeForm.jsx
+++ src/jsx/shared/ItemSizeForm.jsx
@@ -3,14 +3,20 @@ import { sizes } from '../ItemMeta';
 import { createRandomID } from '../../../utils';
 
 export const ItemSizeForm = (props) => {
+    const { handleChange, selectedSize } = props; 
+
     return (
-        <div className="form-group" onChange={props.handleChange}>
+        <div className="form-group" onChange={handleChange}>
             <label>SIZE:</label>
             <select id="size" className="form-control">
                 {
                     Object.keys(sizes).map((size) => {
                         return (
-                            <option value={size} key={createRandomID()} selected={props.selectedSize === size}>
+                            <option 
+                                value={size} 
+                                key={createRandomID()} 
+                                selected={selectedSize === size}
+                            >
                                 {sizes[size]}
                             </option>
                         )
