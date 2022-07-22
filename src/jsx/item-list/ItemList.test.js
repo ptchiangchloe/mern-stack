f61f6189c9d902461d1a8e7e836831857a5cbae2
Hanyu Jiang <hanyujiang5@gmail.com>
2022-07-21 20:15:16 -0700

    implement a async function test by using fetch method

--- src/jsx/item-list/ItemList.test.js
+++ src/jsx/item-list/ItemList.test.js
@@ -1,11 +1,25 @@
 import { loadItemData } from "./ItemList";
+import fetch from 'node-fetch';
 
+// Test fetch from the local server api 
 it("loads item data from server api", async () => {
     const apiURL = 'http://localhost:4000/api/items'
-    const data = await loadItemData(apiURL)
-    console.log(data)
 
-    expect(data.length).not.toBe(0)
+    const result = await fetch(apiURL);
+    const data = await result.json();
 
-    expect(data[0]['brand'].toBe('Hurley'))
+    expect(data.records.length).not.toBe(0)
+
+    const testBrand = data.records[0].brand
+    expect(testBrand).toBe('Hurley')
 })
+
+global.fetch = jest.fn(() => {
+    Promise.resolve({
+        json: () => Promise.resolve({
+
+        })
+    })
+})
+
+
